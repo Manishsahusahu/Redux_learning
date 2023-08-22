da2a33a72970cb6c8a3a7f7c76c3b0b27b0cdc8c
Manishsahusahu <sahumanish327@gmail.com>
2023-08-22 20:18:00 +0530

    Testing of actions using dispatch

--- createStore.js
+++ createStore.js
@@ -1,20 +1,20 @@
 const { createStore } = require("redux");
 
 function todoReducer(state, action) {
-    if (action.type === "add_todo") {
+    if (action.type === "ADD_TODO") {
         const todoText = action.payload.todoText;
         return [
             ...state,
             {
                 text: todoText,
-                isFinished: ƒalse,
+                isFinished: false,
                 id: !state.length ? 1 : state[state.length - 1].id + 1,
             },
         ];
-    } else if (action.type === "delete_todo") {
+    } else if (action.type === "DELETE_TODO") {
         const todo = action.payload.todo;
         return state.filter((element) => element.id !== todo.id);
-    } else if (action.type === "edit_todo") {
+    } else if (action.type === "EDIT_TODO") {
         const { todo, todoText } = action.payload;
         return state.map((el) => {
             if (todo.id === el.id) el.text = todoText;
@@ -26,4 +26,10 @@ function todoReducer(state, action) {
 
 const response = createStore(todoReducer, []);
 
-console.log(response)
+const {dispatch, subscribe, getState, replaceReducer}= response;
+
+dispatch({type: 'ADD_TODO', payload: {todoText: 'todo number1 is here'}})
+dispatch({type: 'ADD_TODO', payload: {todoText: 'todo number2 is here'}})
+// dispatch({type: 'DELETE_TODO', payload: {todo: getState()[0]}})
+dispatch({type: 'EDIT_TODO', payload: {todo: getState()[0], todoText: 'todo number1 is edited here'}})
+console.log(getState())
