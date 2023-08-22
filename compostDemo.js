da7d6fdd1599c76ad75bf541a7b313ae02bfaea2
Manishsahusahu <sahumanish327@gmail.com>
2023-08-22 12:33:42 +0530

    basic functions with traditional method

---
+++ compostDemo.js
@@ -0,0 +1,14 @@
+function removeSpaces(str) {
+  return str.split(" ").join("");
+}
+
+function repeatString(str){
+    return str.repeat(3)
+}
+
+function convertToUpper(str){
+    return str.toUpperCase()
+}
+// console.log(removeSpaces('dkfjd fjdkfjd kdjfkdf '))
+// console.log(repeatString('Ramesh '))
+console.log(convertToUpper('Ramesh '))
